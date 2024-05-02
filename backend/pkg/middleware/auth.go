4d54c3d5391b89ab643b0d378d920aac599e5a6c
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-02 13:42:34 +0300

    adding auth.go

---
+++ backend/pkg/middleware/auth.go
@@ -0,0 +1,32 @@
+// auth.go
+
+package middleware
+
+import (
+    "net/http"
+    "github.com/dgrijalva/jwt-go"
+    // "github.com/RabihSassouh/final-project/backend/pkg/models"
+)
+
+var jwtKey = []byte("your-secret-key")
+
+func AuthenticationMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        tokenString := r.Header.Get("Authorization")
+        if tokenString == "" {
+            w.WriteHeader(http.StatusUnauthorized)
+            return
+        }
+
+        token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+            return jwtKey, nil
+        })
+
+        if err != nil || !token.Valid {
+            w.WriteHeader(http.StatusUnauthorized)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
