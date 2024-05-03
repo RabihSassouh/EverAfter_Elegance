c5cdeea7d167f80d06b90a722f0edd97bc4494ed
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 14:40:46 +0300

    adding title to section1

---
+++ frontend/src/components/Section1.tsx
@@ -0,0 +1,12 @@
+
+function Section1(){
+    return(
+        <div className="bg-white text-gray-800 py-10 px-12">
+        <div className="container mx-auto text-center">
+          <h2 className="text-4xl font-semibold font-poppins mb-5">Ever After Elegance</h2>
+          </div>
+        </div>
+    )
+}
+
+export default Section1;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import NavigationBar from "../components/NavigationBar";
 import Hero from "../components/Hero";
+import Section1 from "../components/Section1";
 
 const Home = () => {
   return (
     <div>
       <NavigationBar />
       <Hero/>
+      <Section1/>
     </div>
   );
 };
