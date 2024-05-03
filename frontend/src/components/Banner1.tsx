e13de520f8aacc8149b8563a19a9232938f5deb0
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:31:18 +0300

    fixing bugs

--- frontend/src/components/Banner1.tsx
+++ frontend/src/components/Banner1.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function HeroBanner() {
   return (
     <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
