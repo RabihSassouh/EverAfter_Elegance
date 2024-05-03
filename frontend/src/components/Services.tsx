66cc2f13f4455c129b4420291aca535794c7be3c
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 21:42:04 +0300

    adding marry me to services

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -13,7 +13,14 @@ function Services() {
         title: "Real Weddings",
         link: "",
         description:  `Explore our real weddings section for heartwarming love stories that will inspire your own big day. From simple celebrations to grand affairs, each one is filled with love and laughter. Get lost in the beautiful moments captured in every photo and let them spark ideas for your own special day!`   
+    },
+    {
+        image: "../../offer3.jpg",    
+        title: "Marry Me",
+        link: "",
+        description:  `Where love stories begin and dreams come true. Whether you're popping the question or planning heartfelt vows, here you'll find all the inspiration you need for unforgettable moments of love and commitment. Let's make your journey to 'I do' truly special.`   
     }
+    
   ];
   return (
     <div className="py-12 px-12 bg-white">
