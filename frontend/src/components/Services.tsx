46ed413bd5f621a15f8cd2ec4425f5c2e651d4ed
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:37:38 +0300

    adding title to services section

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -1,5 +1,13 @@
-function Services(){
-
+function Services() {
+  return (
+    <div className="py-12 px-12 bg-white">
+      <div className="container mx-auto">
+        <h2 className="text-4xl font-semibold font-poppins text-center mb-9">
+          What we offer?
+        </h2>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
