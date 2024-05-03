3ddde11dceb6e71ca2397355adf25176dfc0b77a
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 22:34:09 +0300

    mapping venues

--- frontend/src/components/PopularVenues.tsx
+++ frontend/src/components/PopularVenues.tsx
@@ -5,7 +5,11 @@ function PopularVenues() {
       location: "Brittany",
       image: "../../BlancDeChene.jpg",
     },
-    { name: "Byblos Su Mer", location: "Jbeil", image: "../../ByblosSuMer.jpg" },
+    {
+      name: "Byblos Su Mer",
+      location: "Jbeil",
+      image: "../../ByblosSuMer.jpg",
+    },
     {
       name: "Nuit Blanche",
       location: "Mar Roukoz",
@@ -20,7 +24,28 @@ function PopularVenues() {
         <h2 className="text-4xl font-semibold font-poppins mb-10 text-center">
           Popular Venues
         </h2>
-        
+        <div className="w-full flex flex-col md:flex-row flex-wrap justify-center items-center md:justify-between gap-10 md:gap-0">
+          {venues.map((venue, i) => (
+            <div
+              className="h-80 w-64 relative max-w-xs rounded-lg overflow-hidden hover:shadow-md"
+              key={i}
+            >
+              <img
+                src={venue.image}
+                alt={venue.name}
+                className="w-full h-full object-cover"
+              />
+              <div className="absolute inset-x-0 bottom-0 bg-[#FFFFFFCC] opacity-80 text-white p-4">
+                <h3 className="text-lg font-semibold font-poppins text-center text-black">
+                  {venue.name}
+                </h3>
+                <p className="font-poppins text-center text-sm text-[#3A4850] font-semibold">
+                  {venue.location}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
