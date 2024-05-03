768289e9434708dfe61f22daedc6e5486cb8e71b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:32:30 +0300

    creating banner1 section

--- frontend/src/components/Banner1.tsx
+++ frontend/src/components/Banner1.tsx
@@ -4,7 +4,7 @@ function HeroBanner() {
     <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
         <h1 className="text-5xl font-semibold font-poppins mb-5">Bringing Dreams To Life</h1>
-        <p >Timeless Elegance and Romance</p>
+        <p className="font-poppins font-medium text-[#FFFFFF99]">Timeless Elegance and Romance</p>
       </div>
     </div>
   );
