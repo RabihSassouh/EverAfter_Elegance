5b4e4beca5cb0d420e99814c97caf81a646ef2a2
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 22:49:06 +0300

    adding title to contactUs

--- frontend/src/components/ContactUs.tsx
+++ frontend/src/components/ContactUs.tsx
@@ -1,6 +1,10 @@
-function ContactUs(){
-    return(
-        <div></div>
-    )
+function ContactUs() {
+  return (
+    <div className="px-12 py-12 m-3 rounded-lg flex flex-col justify-center items-center">
+      <h2 className="text-4xl font-semibold font-poppins mb-2">
+        Still Have A Questions?
+      </h2>
+    </div>
+  );
 }
 export default ContactUs;
