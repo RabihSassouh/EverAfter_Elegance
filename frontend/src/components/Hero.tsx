65cd2918d1f39c43aa2b9ba537ffb88b53ab24c9
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 14:22:50 +0300

    adding the search button

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,7 +5,9 @@ const Hero = () => {
         <div className="relative bg-cover bg-center h-[450px]" style={{backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
                 <h1 className="text-white text-4xl font-poppins font-semibold mb-4">Bringing Dreams to life...</h1>
-                <div className="p-4 relative inline-block w-[50%] flex justify-center items-center overflow-hidden w-full">
+                <div className=" p-4 flex justify-center items-center w-full">
+                    <div className=" flex justify-center items-center overflow-hidden w-full">
+                        <div className="relative inline-block w-[50%]">
                             <select className="appearance-none bg-[#FFFFFF0F] px-4 py-2 text-[#FFFFFFCC] focus:outline-none font-poppins w-full rounded-s-lg">
                                 <option>Select Category</option>
                                 {/* Categories of the business to be added */}
@@ -14,7 +16,11 @@ const Hero = () => {
                                 <MdOutlineKeyboardArrowDown className='w-6 h-6'/>
                             </div>
                         </div>
-                        
+                        <button className="bg-primary text-white px-4 py-2 hover:bg-secondary font-poppins rounded-e-lg">
+                            Search
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
