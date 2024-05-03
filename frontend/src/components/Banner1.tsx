0fd08dad0fc0af1f28459d1f1a7332617225e9ca
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:28:54 +0300

    adding background image

---
+++ frontend/src/components/Banner1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroBanner() {
+  return (
+    <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      
+    </div>
+  );
+}
+
+export default HeroBanner;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import NavigationBar from "../components/NavigationBar";
 import Hero from "../components/Hero";
 import Section1 from "../components/Section1";
+import Banner1 from "../components/Banner1"
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <NavigationBar />
       <Hero/>
       <Section1/>
+      <Banner1/>
     </div>
   );
 };
