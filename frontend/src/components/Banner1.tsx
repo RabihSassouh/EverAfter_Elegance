5e128d0ba9939c846978b887bee1e990af39434f
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:31:00 +0300

    editing banner1

--- frontend/src/components/Banner1.tsx
+++ frontend/src/components/Banner1.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function HeroBanner() {
   return (
     <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div >
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
         <h1 className="text-5xl font-semibold font-poppins mb-5">Bringing Dreams To Life</h1>
       </div>
     </div>
