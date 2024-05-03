949f306b5cfd50cb3646887ea6f7dfc45050b04b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:31:54 +0300

    adding text to banner1

--- frontend/src/components/Banner1.tsx
+++ frontend/src/components/Banner1.tsx
@@ -1,8 +1,10 @@
+
 function HeroBanner() {
   return (
     <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
         <h1 className="text-5xl font-semibold font-poppins mb-5">Bringing Dreams To Life</h1>
+        <p >Timeless Elegance and Romance</p>
       </div>
     </div>
   );
