243ecbad21767c9e7ab7286cae32c7ca744d63e5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 15:30:01 +0300

    adding title to banner1

--- frontend/src/components/Banner1.tsx
+++ frontend/src/components/Banner1.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function HeroBanner() {
   return (
     <div className="relative text-white text-center h-60" style={{ backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      
+      <div >
+        <h1 className="text-5xl font-semibold font-poppins mb-5">Bringing Dreams To Life</h1>
+      </div>
     </div>
   );
 }
