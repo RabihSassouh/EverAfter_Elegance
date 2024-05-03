c3f8d8f7596e5222877e13c1891d4b8dd321abb0
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 14:03:15 +0300

    adding the title to hero section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,9 +1,14 @@
-// import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 
 const Hero = () => {
     return (
         <div className="relative bg-cover bg-center h-[450px]" style={{backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center'}}>
             
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
+                
+                <h1 className="text-white text-4xl font-poppins font-semibold mb-4">Bringing Dreams to life...</h1>
+            </div>
+                
         </div>
     )
 }
