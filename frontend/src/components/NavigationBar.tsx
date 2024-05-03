023e20b20faef089568c3388257d37fe6b92fe08
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 13:20:40 +0300

    adding the menuItems

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -2,61 +2,78 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
+  const navigate = useNavigate();
+  const [showMobileNav, setShowMobileNav] = useState(true);
+  const menuItems = [
+    {
+      value: 1,
+      label: "Home",
+      href: "/",
+    },
+    {
+      value: 2,
+      label: "Planning Tools",
+      href: "/",
+    },
+    {
+      value: 3,
+      label: "Venue",
+      href: "/",
+    },
+    {
+      value: 4,
+      label: "Vendors",
+      href: "/",
+    },
+    {
+      value: 5,
+      label: "Offers",
+      href: "/",
+    },
+    {
+      value: 6,
+      label: "Your Business",
+      href: "/",
+    },
+  ];
+  return (
+    <header>
+      <nav className="flex flex-wrap md:flex items-center justify-between w-full py-5 px-12 text-lg text-gray-700 bg-white">
+        {/* logo */}
+        <div>
+          <Link to="/" className="flex justify-center items-center gap-2">
+            <img
+              src="/logo.png"
+              alt="Ever After Elegance Logo"
+              width="72"
+              height="61"
+            />
+            <p className="font-poppins font-medium text-black">
+              Ever After Elegance
+            </p>
+          </Link>
+        </div>
+        <div
+          className={`w-full md:w-auto md:flex md:items-center ${
+            showMobileNav ? "" : "hidden"
+          }`}
+        >
+          <ul className="text-base text-gray-700 pt-4 md:flex md:justify-between md:pt-0">
+            {menuItems.map((item) => (
+              <li key={item.value}>
+                <Link
+                  className="md:p-4 py-2 block font-poppins font-medium text-[#00000099] hover:text-primary"
+                  to={item.href}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-    const navigate = useNavigate();
-    const [showMobileNav, setShowMobileNav] = useState(true);
-    const menuItems = [
-        {
-            value: 1,
-            label: "Home",
-            href: "/"
-        },
-        {
-            value: 2,
-            label: "Planning Tools",
-            href: "/"
-        },
-        {
-            value: 3,
-            label: "Venue",
-            href: "/"
-        },
-        {
-            value: 4,
-            label: "Vendors",
-            href: "/"
-        },
-        {
-            value: 5,
-            label: "Offers",
-            href: "/"
-        },
-        {
-            value: 6,
-            label: "Your Business",
-            href: "/"
-        }
-    ];
-    return(
-        <header>
-        <nav className="flex flex-wrap md:flex items-center justify-between w-full py-5 px-12 text-lg text-gray-700 bg-white">
-          {/* logo */}
-          <div>
-            <Link to="/" className="flex justify-center items-center gap-2">
-              <img
-                src="/logo.png"
-                alt="Ever After Elegance Logo"
-                width="72"
-                height="61"
-              />
-              <p className="font-poppins font-medium text-black">
-                Ever After Elegance
-              </p>
-            </Link>
-          </div>
-          </nav>
-          </header>
-    )
-}
-
-export default NavigationBar
+export default NavigationBar;
