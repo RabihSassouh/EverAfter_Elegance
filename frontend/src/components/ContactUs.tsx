9e7bbb009c48e8a0a036e49a71fbdc3e29b55077
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 22:51:39 +0300

    adding contactUs text

--- frontend/src/components/ContactUs.tsx
+++ frontend/src/components/ContactUs.tsx
@@ -2,8 +2,11 @@ function ContactUs() {
   return (
     <div className="px-12 py-12 m-3 rounded-lg flex flex-col justify-center items-center">
       <h2 className="text-4xl font-semibold font-poppins mb-2">
-        Still Have A Questions?
+        Still Have A Question?
       </h2>
+      <p className="font-poppins font-medium text-sm mb-8 text-[#494949]">
+        Can't find the answer you're looking for?
+      </p>
     </div>
   );
 }
