985bbe1c88eefa23cac65f4c9a70cfa5501d1671
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 14:42:03 +0300

    adding text to section1

--- frontend/src/components/Section1.tsx
+++ frontend/src/components/Section1.tsx
@@ -1,12 +1,20 @@
-
-function Section1(){
-    return(
-        <div className="bg-white text-gray-800 py-10 px-12">
-        <div className="container mx-auto text-center">
-          <h2 className="text-4xl font-semibold font-poppins mb-5">Ever After Elegance</h2>
-          </div>
-        </div>
-    )
+function Section1() {
+  return (
+    <div className="bg-white text-gray-800 py-10 px-12">
+      <div className="container mx-auto text-center">
+        <h2 className="text-4xl font-semibold font-poppins mb-5">
+          Ever After Elegance
+        </h2>
+        <p >
+          We're committed to turning your dream fairytale wedding into reality,
+          with careful attention to every detail. Our goal is to create a
+          memorable experience that reflects your personal style, from grand
+          gestures to small touches. Let us make your special day a magical
+          celebration that you'll remember forever...
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Section1;
