3223041338c6cbf0aff7e09c3ce9f03688bf1c2b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 22:13:53 +0300

    adding title Popular venues

--- frontend/src/components/PopularVenues.tsx
+++ frontend/src/components/PopularVenues.tsx
@@ -1,6 +1,12 @@
-function PopularVenues () {
-    return(
-        <div></div>
-    )
-} 
+function PopularVenues() {
+  return (
+    <div className="py-12 px-12">
+      <div className="mx-auto px-4">
+        <h2 className="text-4xl font-semibold font-poppins mb-10 text-center">
+          Popular Venues
+        </h2>
+      </div>
+    </div>
+  );
+}
 export default PopularVenues;
