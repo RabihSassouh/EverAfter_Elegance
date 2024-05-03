2ce76666c8c59d708a8e4971889450198d0b0ad5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-03 14:11:03 +0300

    editing the select category

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,13 +5,16 @@ const Hero = () => {
         <div className="relative bg-cover bg-center h-[450px]" style={{backgroundImage: "url('../../banner.jpg')", backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
                 <h1 className="text-white text-4xl font-poppins font-semibold mb-4">Bringing Dreams to life...</h1>
-                    <div className="p-4 relative inline-block w-[50%] flex justify-center items-center overflow-hidden w-full">
+                <div className="p-4 relative inline-block w-[50%] flex justify-center items-center overflow-hidden w-full">
                             <select className="appearance-none bg-[#FFFFFF0F] px-4 py-2 text-[#FFFFFFCC] focus:outline-none font-poppins w-full rounded-s-lg">
                                 <option>Select Category</option>
                                 {/* Categories of the business to be added */}
                             </select>
-
-                </div>
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+                                <MdOutlineKeyboardArrowDown className='w-6 h-6'/>
+                            </div>
+                        </div>
+                        
             </div>
         </div>
     )
