b38a36f6646466045edf6dd63aa7531bee9ff6b5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-04 01:22:32 +0300

    adding email phone and address

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -16,6 +16,20 @@ function Footer() {
           </h2>
         </Link>
       </div>
+      <div className='flex flex-col gap-1'>
+                <div className='flex gap-3 text-sm'>
+                    <span className='font-medium font-poppins'>Email:</span>
+                    <p className='font-semibold font-poppins'>everafterelegance@contact.com</p>
+                </div>
+                <div className='flex gap-3 text-sm'>
+                    <span className='font-medium font-poppins'>Phone:</span>
+                    <p className='font-semibold font-poppins'>+961-03-550448</p>
+                </div>
+                <div className='flex gap-3 text-sm'>
+                    <span className='font-medium font-poppins'>Address:</span>
+                    <p className='font-semibold font-poppins'>Beirut, Lebanon</p>
+                </div>
+            </div>
     </footer>
   );
 }
