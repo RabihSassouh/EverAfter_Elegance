863f1644a75c18b825879ade04f37d3afc159cf5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-04 13:04:38 +0300

    adding signup image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
-import Login  from "./pages/Authentication/Login";
+import SignUp from "./pages/Authentication/SignUp";
+// import Login  from "./pages/Authentication/Login";
 // import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       {/* <Home /> */}
-      <Login/>
+      {/* <Login/> */}
+      <SignUp/>
     </Router>
   );
 }
---
+++ frontend/src/pages/Authentication/SignUp.tsx
@@ -0,0 +1,16 @@
+function SignUp() {
+  return (
+    <div className="min-h-screen flex">
+      <div className="hidden md:block md:w-1/2">
+        <div className="relative w-full h-full">
+          <img
+            src="../../offer2.jpg"
+            alt="Wedding"
+            className="w-full h-full object-cover rounded-tl-32 rounded-bl-32"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SignUp;
