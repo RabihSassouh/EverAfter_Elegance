ad6f5864f30c533d781ffc374875ec6f7bd15953
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-04 21:52:16 +0300

    creating validation for CreateUser

--- backend/pkg/controllers/users-controller.go
+++ backend/pkg/controllers/users-controller.go
@@ -1,54 +1,114 @@
 package controllers
 
-import(
+import (
 	"encoding/json"
 	"fmt"
+	"github.com/RabihSassouh/final-project/backend/pkg/models"
+	"github.com/RabihSassouh/final-project/backend/pkg/utils"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
-	"github.com/RabihSassouh/final-project/backend/pkg/utils"
-	"github.com/RabihSassouh/final-project/backend/pkg/models"
 )
 
 var NewUser models.User
 
-func CreateUser(w http.ResponseWriter, r *http.Request){
-	CreateUser := &models.User{}
-	utils.ParseBody(r, CreateUser)
-	u := CreateUser.CreateUser()
-	res, _ := json.Marshal(u)
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	newUser := &models.User{}
+	utils.ParseBody(r, newUser)
+
+	// Check if the email already exists
+	existingUser, err := models.GetUserByEmail(newUser.Email)
+	if err != nil {
+		errorMessage := map[string]string{"error": "error checking email existence"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(res)
+		return
+	}
+	if existingUser != nil {
+		errorMessage := map[string]string{"error": "email already used"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+	// Check if the firstname is filled
+	if newUser.FirstName == "" {
+		errorMessage := map[string]string{"error": "firstname is required"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// Check if the lastname is filled
+	if newUser.LastName == "" {
+		errorMessage := map[string]string{"error": "lastname is required"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// Check if the password is filled
+	if newUser.Password == "" {
+		errorMessage := map[string]string{"error": "password is required"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// Check if the password is longer than 8 characters
+	if len(newUser.Password) < 8 {
+		errorMessage := map[string]string{"error": "password should be at least 8 characters"}
+		res, _ := json.Marshal(errorMessage)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(res)
+		return
+	}
+
+	// Create the user
+	createdUser := newUser.CreateUser()
+	res, _ := json.Marshal(createdUser)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
 
-func GetUsers(w http.ResponseWriter, r *http.Request){
-	NewUsers:= models.GetAllUsers()
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	NewUsers := models.GetAllUsers()
 	res, _ := json.Marshal(NewUsers)
-	w.Header().Set("Content-Type","pkglication/json")
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
- 
-func GetUserById(w http.ResponseWriter, r *http.Request){
+
+func GetUserById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	userId := vars["userId"]
-	ID, err := strconv.ParseInt(userId,0,0)
+	ID, err := strconv.ParseInt(userId, 0, 0)
 	if err != nil {
 		fmt.Println("error while parsing")
 	}
 	userDetails, _ := models.GetUserById(ID)
 	res, _ := json.Marshal(userDetails)
-	w.Header().Set("Content-Type","pkglication/json")
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
 
-
-func DeleteUser(w http.ResponseWriter, r *http.Request){
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	userId := vars["userId"]
-	ID, err := strconv.ParseInt(userId,0,0)
-	if err != nil{
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
 		fmt.Println("error while parsing")
 	}
 	user := models.DeleteUser(ID)
@@ -58,26 +118,26 @@ func DeleteUser(w http.ResponseWriter, r *http.Request){
 	w.Write(res)
 }
 
-func UpdateUser(w http.ResponseWriter, r *http.Request){
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	var updateUser = &models.User{}
 	utils.ParseBody(r, updateUser)
 	vars := mux.Vars(r)
 	userId := vars["userId"]
-	ID, err := strconv.ParseInt(userId,0,0)
+	ID, err := strconv.ParseInt(userId, 0, 0)
 	if err != nil {
 		fmt.Println("error while parsing")
 	}
-	userDetails, db:= models.GetUserById(ID)
-	if updateUser.FirstName != ""{
-	userDetails.FirstName = updateUser.FirstName
+	userDetails, db := models.GetUserById(ID)
+	if updateUser.FirstName != "" {
+		userDetails.FirstName = updateUser.FirstName
 	}
-	if updateUser.LastName != ""{
+	if updateUser.LastName != "" {
 		userDetails.LastName = updateUser.LastName
 	}
-	if updateUser.Email != ""{
-		userDetails.Email= updateUser.Email
+	if updateUser.Email != "" {
+		userDetails.Email = updateUser.Email
 	}
-	if updateUser.Password != ""{
+	if updateUser.Password != "" {
 		userDetails.Password = updateUser.Password
 	}
 	db.Save(&userDetails)
--- backend/pkg/models/users.go
+++ backend/pkg/models/users.go
@@ -4,6 +4,8 @@ import (
 	"github.com/RabihSassouh/final-project/backend/pkg/config"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
+	"errors"
+	"regexp"
 )
 
 var db *gorm.DB
@@ -22,16 +24,79 @@ func init(){
 	db.AutoMigrate(&User{})
 }
 
-func (u *User) CreateUser() *User{
-	hashPassword, err := hashPassword(u.Password)
-	if err != nil {
-		return nil
-	}
-	u.Password = hashPassword
-	db.NewRecord(u)
-	db.Create(&u)
-	return u
+// func (u *User) CreateUser() *User{
+// 	hashPassword, err := hashPassword(u.Password)
+// 	if err != nil {
+// 		return nil
+// 	}
+// 	u.Password = hashPassword
+// 	db.NewRecord(u)
+// 	db.Create(&u)
+// 	return u
+// }
+
+func (u *User) CreateUser() *User {
+    // Validate the user object
+    if err := u.validate(); err != nil {
+        // Validation failed, return nil indicating failure
+        return nil
+    }
+
+    // Hash the password
+    hashedPassword, err := hashPassword(u.Password)
+    if err != nil {
+        // Error hashing password, return nil indicating failure
+        return nil
+    }
+    u.Password = hashedPassword
+
+    // Create the user in the database
+    db.NewRecord(u)
+    if err := db.Create(&u).Error; err != nil {
+        // Error creating user, return nil indicating failure
+        return nil
+    }
+
+    // Return the created user
+    return u
 }
+
+
+func (u *User) validate() error {
+	if u.Email == "" {
+		return errors.New("email is required")
+	}
+
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	if !emailRegex.MatchString(u.Email) {
+		return errors.New("invalid email address format")
+	}
+	
+	if u.Password == "" {
+		return errors.New("password is required")
+	}
+
+	if len(u.Password) < 8 {
+		return errors.New("password must be at least 8 characters long")
+	}
+	return nil
+}
+
+func GetUserByEmail(email string) (*User, error) {
+    var user User
+    result := db.Where("email = ?", email).First(&user)
+    if result.Error != nil {
+        if result.RecordNotFound() {
+            // User with the provided email does not exist
+            return nil, nil
+        }
+        // Some other error occurred
+        return nil, result.Error
+    }
+    // User with the provided email found
+    return &user, nil
+}
+
 
 func hashPassword(password string) (string, error){
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
