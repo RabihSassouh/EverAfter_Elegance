36e9246428a520e1bccf7b77a331349d85c63fcf
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-04 13:09:28 +0300

    adding back button

--- frontend/src/pages/Authentication/SignUp.tsx
+++ frontend/src/pages/Authentication/SignUp.tsx
@@ -1,6 +1,22 @@
+// import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
+import { MdChevronLeft } from "react-icons/md";
+
 function SignUp() {
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+      navigate(-1);
+    };
   return (
     <div className="min-h-screen flex">
+      <button
+        className="w-full flex items-center justify-start font-poppins text-primary font-medium hover:text-secondary"
+        onClick={handleGoBack}
+      >
+        <MdChevronLeft className="h-6 w-6" />
+        Back
+      </button>
       <div className="hidden md:block md:w-1/2">
         <div className="relative w-full h-full">
           <img
