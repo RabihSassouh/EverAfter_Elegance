ad40b964553595616c31d888d0e52a61a3034c7e
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-05 14:27:35 +0300

    adding routes to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import SignUp from "./pages/Authentication/SignUp";
-// import Login  from "./pages/Authentication/Login";
-// import Home from "./pages/Home";
+import Login from "./pages/Authentication/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
-      {/* <Home /> */}
-      {/* <Login/> */}
-      <SignUp/>
+      <Routes>
+        <Route element={<Login />} path="/login"></Route>
+        <Route element={<SignUp />} path="/sign-up"></Route>
+        <Route element={<Home />} path="/"></Route>
+      </Routes>
     </Router>
   );
 }
