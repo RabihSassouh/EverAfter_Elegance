0b513c21b8da649d841b0d4e6c5148b813b441c9
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-06 02:34:18 +0300

    frontend: creating the categories search

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import './scrollBar.css'
 
 const root = ReactDOM as any;
 root.createRoot(document.getElementById("root")).render(
