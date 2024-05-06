a701145ae6c33e59c70156b211572644408db1f8
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-06 23:12:48 +0300

    backend: implementing CORS middlewar in routes

--- backend/pkg/routes/users-routes.go
+++ backend/pkg/routes/users-routes.go
@@ -8,10 +8,11 @@ import (
 
 var RegisterUserRoutes = func (router *mux.Router)  {
 	router.HandleFunc("/signup/", controllers.CreateUser).Methods("POST")
-	router.HandleFunc("/login/", controllers.GetUserById).Methods("POST")
-	router.HandleFunc("/user/", middleware.AuthenticationMiddleware(controllers.GetUsers)).Methods("GET")
+	router.HandleFunc("/api/login/", controllers.GetUserById).Methods("POST")
+	router.HandleFunc("/api/user/", controllers.GetUsers).Methods("GET")
 	// router.HandleFunc("/user/{userId}", middleware.AuthenticationMiddleware(controllers.GetUserById)).Methods("GET")
 	router.HandleFunc("/user/{userId}", middleware.AuthenticationMiddleware(controllers.UpdateUser)).Methods("PUT")
 	router.HandleFunc("/user/{userId}", middleware.AuthenticationMiddleware(controllers.DeleteUser)).Methods("DELETE")
+	router.Use(middleware.CORSMiddleware)
 
 }
