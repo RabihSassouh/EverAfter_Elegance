3c4dfdda82bee1cef698b4407a00bae5a0887eba
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-07 00:29:42 +0300

    frontend: creating venue page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
 import Home from "./pages/Home";
+import Venue from "./pages/Vendors";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route element={<Login />} path="/login"></Route>
         <Route element={<SignUp />} path="/sign-up"></Route>
         <Route element={<Home />} path="/"></Route>
+        <Route element={<Venue />} path="/venue"></Route>
       </Routes>
     </Router>
   );
