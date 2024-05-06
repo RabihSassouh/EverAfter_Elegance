f3fe30d9792c0dfca18b1b3a13c50a6463fec1ab
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-06 23:13:17 +0300

    backend: adding handler for the middleware

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -7,6 +7,7 @@ import (
 	_ `github.com/jinzhu/gorm/dialects/mysql`
 	"github.com/RabihSassouh/final-project/backend/pkg/routes"
 	// "github.com/go-sql-driver/mysql"
+	"github.com/RabihSassouh/final-project/backend/pkg/middleware" 
 )
 
 func main() {
@@ -18,6 +19,30 @@ func main() {
 	routes.RegisterVendorsRoutes(r)
 	routes.RegisterGuestsRoutes(r)
 	routes.RegisterGuestbookRoutes(r)
+	handler := middleware.CORSMiddleware(r)
 	http.Handle("/",r)
-	log.Fatal(http.ListenAndServe("localhost:8080",r))
+	log.Fatal(http.ListenAndServe("localhost:8080",handler))
+	
 }
+
+// import (
+//     "net/http"
+//     "yourapp/controllers"
+//     "yourapp/middleware"
+
+// func main() {
+//     mux := http.NewServeMux()
+
+    // Routes
+    // mux.HandleFunc("/login", controllers.LoginHandler)
+    // mux.HandleFunc("/other_api", controllers.OtherAPIHandler)
+
+    // Apply middleware
+    // handler := middleware.CORSMiddleware(mux)
+
+    // Start server
+    // server := &http.Server{
+    //     Addr:    ":8080",
+    //     Handler: handler,
+    // }
+    // server.ListenAndServe()
