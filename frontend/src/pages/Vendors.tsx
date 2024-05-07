65892887be071afee8673b075b41e8c714d28983
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-07 19:51:18 +0300

    frontend: adding text banner

--- frontend/src/pages/Vendors.tsx
+++ frontend/src/pages/Vendors.tsx
@@ -13,6 +13,13 @@ const Venue = () => {
             <h2 className="text-4xl font-semibold font-poppins mb-5 text-[#121212]">
               Ever After Elegance
             </h2>
+            <p className="font-poppins text-[#494949]">
+              We're committed to turning your dream fairytale wedding into
+              reality, with careful attention to every detail. Our goal is to
+              create a memorable experience that reflects your personal style,
+              from grand gestures to small touches. Let us make your special day
+              a magical celebration that you'll remember forever...
+            </p>
         </div>
     )
 }
