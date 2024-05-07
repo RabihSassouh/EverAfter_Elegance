8d9c5f5b88eba27097f9e516cec687f36d52d66a
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-08 00:17:08 +0300

    adding single vendor page

---
+++ frontend/src/pages/SingleVendor.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+
+const SingleVenue: React.FC = () => {
+
+    return (
+        <div>
+            <NavigationBar/>
+        </div>
+    );
+};
+
+export default SingleVenue;
