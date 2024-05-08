4e769e23f0ab0c4a026a550525ef438dce1d8260
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-09 01:59:18 +0300

    backend: fixing usertype model

--- backend/pkg/models/userstype.go
+++ backend/pkg/models/userstype.go
@@ -1,34 +1,61 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-)
+// import (
+// 	"github.com/jinzhu/gorm"
+// 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/config"
 
-type Userstype struct {
-	gorm.Model
-	UserType string `json:"user_type"`
-	UserID   uint   `json:"user_id"`
-}
+// )
 
-func (s *Userstype) CreateUserstype() *Userstype {
-	db.Create(&s)
-	return s
-}
+// type Userstype struct {
+// 	gorm.Model
+// 	UserType string `json:"user_type"`
+// 	UserID   uint   `json:"user_id"`
+// }
 
-func GetAllUserstypes() []Userstype {
-	var Userstypes []Userstype
-	db.Find(&Userstypes)
-	return Userstypes
-}
+// func init() {
+// 	config.Connect()
+// 	db = config.GetDB()
+// 	db.AutoMigrate(&Userstype{})
+// }
 
-func GetUserstypeByID(ID uint) (*Userstype, *gorm.DB) {
-	var getUserstype Userstype
-	db := db.Where("ID=?", ID).Find(&getUserstype)
-	return &getUserstype, db
-}
 
-func DeleteUserstype(ID uint) Userstype {
-	var userstype Userstype
-	db.Where("ID=?", ID).Delete(userstype)
-	return userstype
-}
+// func (s *Userstype) CreateUserstype() *Userstype {
+// 	db.Create(&s)
+// 	return s
+// }
+
+// func GetAllUserstypes() []Userstype {
+// 	var Userstypes []Userstype
+// 	db.Find(&Userstypes)
+// 	return Userstypes
+// }
+
+// func GetUserstypeByID(ID uint) (*Userstype, *gorm.DB) {
+// 	var getUserstype Userstype
+// 	db := db.Where("ID=?", ID).Find(&getUserstype)
+// 	return &getUserstype, db
+// }
+
+// func DeleteUserstype(ID uint) Userstype {
+// 	var userstype Userstype
+// 	db.Where("ID=?", ID).Delete(userstype)
+// 	return userstype
+// }
+
+// type UserType struct {
+//     gorm.Model
+//     UserType string `gorm:"unique"`
+// }
+
+// func CreateUserType(userType string) (uint, error) {
+//     // Create a new instance of UserType
+//     userTypeObj := &UserType{UserType: userType}
+
+//     // Create the user type record in the database
+//     if err := db.Create(userTypeObj).Error; err != nil {
+//         return 0, err
+//     }
+
+//     // Return the ID of the newly created user type
+//     return userTypeObj.ID, nil
+// }
--- backend/pkg/routes/userstype-routes.go
+++ backend/pkg/routes/userstype-routes.go
@@ -1,13 +1,13 @@
 package routes
 
-import (
-	"github.com/gorilla/mux"
-	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/controllers"
-)
+// import (
+// 	"github.com/gorilla/mux"
+// 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/controllers"
+// )
 
-var RegisterUserstypeRoutes = func(router *mux.Router) {
-	router.HandleFunc("/userstype/", controllers.CreateUserstype).Methods("POST")
-	router.HandleFunc("/userstype", controllers.GetAllUserstypes).Methods("GET")
-	router.HandleFunc("/userstype/{userTypeID}", controllers.GetUserstypeByID).Methods("GET")
-	router.HandleFunc("/userstype/{userTypeID}", controllers.DeleteUserstype).Methods("DELETE")
-}
+// var RegisterUserstypeRoutes = func(router *mux.Router) {
+// 	router.HandleFunc("/userstype/", controllers.CreateUserstype).Methods("POST")
+// 	router.HandleFunc("/userstype", controllers.GetAllUserstypes).Methods("GET")
+// 	router.HandleFunc("/userstype/{userTypeID}", controllers.GetUserstypeByID).Methods("GET")
+// 	router.HandleFunc("/userstype/{userTypeID}", controllers.DeleteUserstype).Methods("DELETE")
+// }
