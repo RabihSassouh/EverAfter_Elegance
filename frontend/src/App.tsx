68213811c9bef1da7e237fd3d845ef8d62c9f22b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-10 18:48:55 +0300

    frontend: adding chatscope packages for ai

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Venue from "./pages/Vendors";
 import SingleVenue from "./pages/SingleVendor";
-import Car1 from "./pages/Test";
+// import Car1 from "./pages/Test";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route element={<Login />} path="/login"></Route>
         <Route element={<SignUp />} path="/sign-up"></Route>
-        <Route element={<Car1/>} path="/"></Route>
+        <Route element={<Home/>} path="/"></Route>
         <Route element={<Venue />} path="/venue"></Route>
         <Route element={<SingleVenue />} path="/v"></Route>
       </Routes>
@@ -22,3 +22,5 @@ function App() {
 }
 
 export default App;
+
+//Ai API secret key: "sk-proj-UW5T6Jl7Bzk96HpGnDnkT3BlbkFJC7o77NiwVqD46H8kZqkQ"
