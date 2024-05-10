b4be04440c36efe530df04a957da3843eea19f90
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-10 11:52:03 +0300

    frontend: adding group to mersedes

--- frontend/public/Mersedes.tsx
+++ frontend/public/Mersedes.tsx
@@ -38,6 +38,13 @@ export default function Model(props: ModelProps): JSX.Element {
           <mesh geometry={nodes.sw221_taillightglass_L_fl_sw221_glass_0.geometry} material={materials.sw221_glass} />
           <mesh geometry={nodes.sw221_taillightglass_L_fl_sw221_redglass_0.geometry} material={materials.sw221_redglass} />
         </group>
+        <group position={[5.268, -3.629, -2.714]} rotation={[-Math.PI / 2, 0, 0]} scale={100}>
+          <mesh geometry={nodes.sw221_taillight_R_fl_sw221_0.geometry} material={materials.sw221} />
+          <mesh geometry={nodes.sw221_taillight_R_fl_sw221_lights_lod0_0.geometry} material={materials.sw221_lights_lod0} />
+          <mesh geometry={nodes.sw221_taillight_R_fl_sw221_runninglight_fl_0.geometry} material={materials.sw221_runninglight_fl} />
+          <mesh geometry={nodes.sw221_taillight_R_fl_sw221_reverselight_fl_0.geometry} material={materials.sw221_reverselight_fl} />
+          <mesh geometry={nodes.sw221_taillight_R_fl_sw221_lights_lod0_trans_0.geometry} material={materials.sw221_lights_lod0_trans} />
+        </group>
 
               </group>
     </group>
