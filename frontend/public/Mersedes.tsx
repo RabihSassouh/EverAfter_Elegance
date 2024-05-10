2aa6157ec7779f8eef897e6c58c1b7b5763bcef5
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-10 11:57:57 +0300

    frontend: adding positioning to mesh

--- frontend/public/Mersedes.tsx
+++ frontend/public/Mersedes.tsx
@@ -92,6 +92,14 @@ export default function Model(props: ModelProps): JSX.Element {
           <mesh geometry={nodes.sw221_mirror_R_fl_sw221_metal_0.geometry} material={materials.sw221_metal} />
           <mesh geometry={nodes.sw221_mirror_R_fl_sw221_signal_R_mirror_fl_0.geometry} material={materials.sw221_signal_R_mirror_fl} />
         </group>
+        <group position={[96.773, 97.887, 41.299]} rotation={[-Math.PI / 2, 0, -0.698]} scale={100}>
+          <mesh geometry={nodes.sw221_mirror_L_fl_sw221_0.geometry} material={materials.sw221} />
+          <mesh geometry={nodes.sw221_mirror_L_fl_sw221_b_0.geometry} material={materials.sw221_b} />
+          <mesh geometry={nodes.sw221_mirror_L_fl_mirror_0.geometry} material={materials.mirror} />
+          <mesh geometry={nodes.sw221_mirror_L_fl_sw221_paint_0.geometry} material={materials.sw221_paint} />
+          <mesh geometry={nodes.sw221_mirror_L_fl_sw221_signal_L_mirror_fl_0.geometry} material={materials.sw221_signal_L_mirror_fl} />
+          <mesh geometry={nodes.sw221_mirror_L_fl_sw221_metal_0.geometry} material={materials.sw221_metal} />
+        </group>
 
               </group>
     </group>
