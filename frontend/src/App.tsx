5b59e22e353b0fec2398ee8d21867c1907c6b293
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-11 17:13:06 +0300

    frontend: adding useContext to get the venuesData

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,50 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import Venue from "./pages/Vendors";
 import SingleVenue from "./pages/SingleVendor";
-import AiTest from "./pages/AiTest";
+// import AiTest from "./pages/AiTest";
 // import Car1 from "./pages/Test";
+import React, {useState} from "react";
+
+
+export interface VenueData {
+   name: string;
+    guests: string;
+    location: string;
+    rating: number;
+    reviewCount: number;
+    imageUrl: string;
+    slug: string;
+  
+}
+export interface ContextType {
+  venuesData: VenueData[];
+  setVenuesData: React.Dispatch<React.SetStateAction<VenueData[]>>;
+}
+
+export const Context = React.createContext<ContextType>({
+  venuesData: [],
+  setVenuesData: () => {},
+});
 
 function App() {
+  const [venuesData, setVenuesData] = useState<VenueData[]>([]);
+
   return (
-    <Router>
-      <Routes>
-        <Route element={<Login />} path="/login"></Route>
-        <Route element={<SignUp />} path="/sign-up"></Route>
-        <Route element={<AiTest/>} path="/"></Route>
-        <Route element={<Venue />} path="/venue"></Route>
-        <Route element={<SingleVenue />} path="/v"></Route>
-      </Routes>
-    </Router>
+    <Context.Provider value={{venuesData, setVenuesData}}>
+      <Router>
+        <Routes>
+          <Route element={<Login />} path="/login"></Route>
+          <Route element={<SignUp />} path="/sign-up"></Route>
+          <Route element={<Home />} path="/"></Route>
+          <Route element={<Venue />} path="/venue"></Route>
+          <Route element={<SingleVenue />} path="/v"></Route>
+        </Routes>
+      </Router>
+      </Context.Provider>
   );
 }
 
 export default App;
-
-//Ai API secret key: "sk-proj-UW5T6Jl7Bzk96HpGnDnkT3BlbkFJC7o77NiwVqD46H8kZqkQ"
