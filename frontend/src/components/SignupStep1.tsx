5e6868905de18f2bea41598e6be7ee62cd550a12
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 02:44:26 +0300

    frontend: fixing the image and next button position

--- frontend/src/components/SignupStep1.tsx
+++ frontend/src/components/SignupStep1.tsx
@@ -5,11 +5,14 @@ const Step1 = () => {
 
   return (
     <div className='w-full h-screen flex items-center justify-center'>
+        <div className='absolute w-[55%] h-full right-0 hidden md:block'>
+          <div className='relative h-full w-full'>
             <div className="absolute inset-0 bg-[#00000066] rounded-xl opacity-70"></div>
             <img src="../../signUp2.png" alt="Couple Image" className="object-cover h-full w-full rounded-xl"/>
             <button className="absolute bottom-5 right-5 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors">Next</button>
-          
-
+          </div>
+        </div>
+      
     </div>
   )
 }
