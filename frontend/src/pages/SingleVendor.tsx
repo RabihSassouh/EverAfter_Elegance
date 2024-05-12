a4dfac327bd799d832fe274e6081b5d6b4225379
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-12 15:41:41 +0300

    frontend: adding footer to single venue page

--- frontend/src/pages/SingleVendor.tsx
+++ frontend/src/pages/SingleVendor.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
-import NavigationBar from '../components/NavigationBar';
-import Slider from '../components/Slider';
-// import VenueDetails from '../components/VendorDetails';
+import React, { useEffect } from "react";
+import NavigationBar from "../components/NavigationBar";
+import Slider from "../components/Slider";
+import VenueDetails from "../components/VendorDetails";
+// import { string } from 'three/examples/jsm/nodes/Nodes.js';
+
+import { useParams } from "react-router-dom";
+import Footer from "../components/Footer";
 // import Venue from './Vendors';
+
+interface VenueData {
+  name: string;
+  guests: number;
+  rating: number;
+  reviewCount: number;
+  description: string;
+  facilities: string[];
+  special_offers: string[];
+  slug: string;
+}
 
 const SingleVenue: React.FC = () => {
 
-    return (
-        <div>
-            <NavigationBar/>
-            <Slider/>
-            {/* <VenueDetails venue={Venue}/> */}
-        </div>
-    );
+
+  return (
+    <div>
+      <NavigationBar />
+      <Slider />
+      <VenueDetails venue={venue} />
+      <Footer />
+    </div>
+  );
 };
 
 export default SingleVenue;
