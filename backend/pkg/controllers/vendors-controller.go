fd40097b2fae3a3519299a09616deee79df9c7c2
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-12 13:16:17 +0300

    backend: adding func upload image to vendors controller

--- backend/pkg/controllers/vendors-controller.go
+++ backend/pkg/controllers/vendors-controller.go
@@ -4,26 +4,110 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"path/filepath"
 	"strconv"
-	"github.com/gorilla/mux"
+
 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/models"
 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/utils"
+	"io"
+	"github.com/gorilla/mux"
+	"os"
 )
 
-func CreateVendor(w http.ResponseWriter, r *http.Request) {
-    // Parse vendor-specific information from the request body
-    vendorInfo := &models.Vendor{}
-    utils.ParseBody(r, vendorInfo)
+var uploadsDir = filepath.Join("backend", "uploads")
 
-    // Create a new vendor object and save it to the database
-    createdVendor := vendorInfo.CreateVendor()
-    
-    // Return the created vendor object in the response
-    res, _ := json.Marshal(createdVendor)
+// CreateVendor creates a new vendor
+
+func UploadImage(w http.ResponseWriter, r *http.Request) {
+    err := r.ParseMultipartForm(10 << 20) // 10 MB max file size
+    if err != nil {
+        http.Error(w, "failed to parse multipart form", http.StatusInternalServerError)
+        return
+    }
+    files := r.MultipartForm.File["images"]
+
+    var imagePaths []string
+
+    for _, file := range files {
+        // Upload each image
+		filepath := filepath.Join(uploadsDir, file.Filename)
+        src, err := file.Open()
+        if err != nil {
+            http.Error(w, "failed to open file", http.StatusInternalServerError)
+            return
+        }
+        defer src.Close()
+		fmt.Println(filepath) 
+        dst, err := os.Create(filepath)
+        if err != nil {
+			fmt.Println(filepath) 
+            http.Error(w, "failed to create file", http.StatusInternalServerError)
+            return
+        }
+        defer dst.Close()
+
+        if _, err := io.Copy(dst, src); err != nil {
+            http.Error(w, "failed to save image", http.StatusInternalServerError)
+            return
+        }
+        imagePaths = append(imagePaths, filepath)
+    }
+
     w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(res)
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(imagePaths)
 }
+
+func CreateVendor(w http.ResponseWriter, r *http.Request) {
+    var vendor models.Vendor
+    if err := json.NewDecoder(r.Body).Decode(&vendor); err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    // Process image uploading
+    err := r.ParseMultipartForm(10 << 20) // 10 MB max file size
+    if err != nil {
+        http.Error(w, "failed to parse multipart form", http.StatusInternalServerError)
+        return
+    }
+    files := r.MultipartForm.File["images"]
+
+    for _, file := range files {
+        // Upload each image
+        _, err := vendor.UploadImage(file)
+        if err != nil {
+            http.Error(w, "failed to upload image", http.StatusInternalServerError)
+            return
+        }
+    }
+
+    // Save the vendor to the database
+    if err := vendor.CreateVendor(); err != nil {
+        http.Error(w, "failed to create vendor", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(vendor)
+}
+
+
+// func CreateVendor(w http.ResponseWriter, r *http.Request) {
+//     // Parse vendor-specific information from the request body
+//     vendorInfo := &models.Vendor{}
+//     utils.ParseBody(r, vendorInfo)
+
+//     // Create a new vendor object and save it to the database
+//     createdVendor := vendorInfo.CreateVendor()
+    
+//     // Return the created vendor object in the response
+//     res, _ := json.Marshal(createdVendor)
+//     w.Header().Set("Content-Type", "application/json")
+//     w.WriteHeader(http.StatusOK)
+//     w.Write(res)
+// }
 
 func GetAllVendors(w http.ResponseWriter, r *http.Request) {
 	vendors := models.GetAllVendors()
@@ -92,9 +176,9 @@ func UpdateVendor(w http.ResponseWriter, r *http.Request) {
 	if updateVendor.Category != "" {
 		vendorDetails.Category = updateVendor.Category
 	}
-	if updateVendor.Images != "" {
-		vendorDetails.Images = updateVendor.Images
-	}
+	// if len(updateVendor.Images) != 0 {
+	// 	vendorDetails.Images = updateVendor.Images
+	// }	
 	if updateVendor.BookingInfo != "" {
 		vendorDetails.BookingInfo = updateVendor.BookingInfo
 	}
@@ -107,8 +191,8 @@ func UpdateVendor(w http.ResponseWriter, r *http.Request) {
 	if updateVendor.Rating != "" {
 		vendorDetails.Rating = updateVendor.Rating
 	}        
-	if updateVendor.Review_count != "" {
-		vendorDetails.Review_count = updateVendor.Review_count
+	if updateVendor.ReviewCount != "" {
+		vendorDetails.ReviewCount = updateVendor.ReviewCount
 	}  
 	if updateVendor.Description  != "" {
 		vendorDetails.Description = updateVendor.Description
