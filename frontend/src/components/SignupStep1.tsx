2d56db9082004b96e913a5a09411f6c48fc06675
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 02:39:21 +0300

    frontend: adding image to step1 in the signup

---
+++ frontend/src/components/SignupStep1.tsx
@@ -0,0 +1,14 @@
+
+const Step1 = () => {
+
+
+
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+      <img src="../../signUp2.png" alt="Couple Image" className="object-cover h-full w-full rounded-xl"/>
+            
+    </div>
+  )
+}
+
+export default Step1
