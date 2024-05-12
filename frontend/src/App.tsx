8621aac6a42d237540e016591d0e35001fdab8e9
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-12 23:32:42 +0300

    frontend: editing dress3d model

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SignUp from "./pages/Authentication/SignUp";
 import Login from "./pages/Authentication/Login";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Venue from "./pages/Vendors";
 import SingleVenue from "./pages/SingleVendor";
 // import AiTest from "./pages/AiTest";
 // import Car1 from "./pages/Test";
 import React, {useState} from "react";
+import Dress1 from "./components/Dress3d";
 
 
 export interface VenueData {
@@ -39,7 +40,7 @@ function App() {
         <Routes>
           <Route element={<Login />} path="/login"></Route>
           <Route element={<SignUp />} path="/sign-up"></Route>
-          <Route element={<Home />} path="/"></Route>
+          <Route element={<Dress1 />} path="/"></Route>
           <Route element={<Venue />} path="/venue"></Route>
           <Route element={<SingleVenue />} path="/venue/:slug"></Route>
         </Routes>
--- frontend/src/components/Dress3d.tsx
+++ frontend/src/components/Dress3d.tsx
@@ -1,69 +1,33 @@
-import * as React from 'react';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-interface ThreeSceneProps {
-    width: number;
-    height: number;
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props: any) {
+  const { scene } = useGLTF("/wedding_dress_3d.glb");
+  return <primitive object={scene} {...props} />;
 }
 
-export default class ThreeScene extends React.Component<ThreeSceneProps> {
-    private renderer: THREE.WebGLRenderer;
-    private camera: THREE.PerspectiveCamera;
-    private scene: THREE.Scene;
-    private mount: HTMLDivElement | null = null; 
-
-    constructor(props: ThreeSceneProps) {
-        super(props);
-
-        // Create a scene
-        this.scene = new THREE.Scene();
-
-        // Create a camera
-        this.camera = new THREE.PerspectiveCamera(75, props.width / props.height, 0.1, 1000);
-        this.camera.position.z = 5;
-
-        // Create a renderer
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(props.width, props.height);
-
-        const spotlight= new THREE.SpotLight(0xffffff)
-        spotlight.position.set(-40,60,-10)
-        this.scene.add(spotlight)
-        // Load the model
-        const loader = new GLTFLoader();
-        loader.load('/wedding_dress_3d.glb', (gltf) => {
-            this.scene.add(gltf.scene);
-        });
-        loader.load('/wedding_dress_3d.glb', (gltf) => {
-            gltf.scene.traverse((child) => {
-                if (child instanceof THREE.Mesh) {
-                    // Check if the mesh belongs to the dress
-                    if (child.name === 'wedding_dress_3d') { // Replace 'dress' with the name of your dress mesh
-                        // Set material color to white
-                        child.material.color.set(0xffffff);
-                    }
-                }
-            });
-            this.scene.add(gltf.scene);
-        });
-        
-    }
-
-    componentDidMount() {
-        // Append renderer to the DOM
-        this.mount?.appendChild(this.renderer.domElement);
-        // this.renderer.setClearColor(0xffffff);
-        // Start the animation loop
-        this.animate();
-    }
-
-    animate = () => {
-        requestAnimationFrame(this.animate);
-        this.renderer.render(this.scene, this.camera);
-    };
-
-    render() {
-        return <div ref={(ref) => (this.mount = ref)} />;
-    }
+function Dress1() {
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      shadows
+      camera={{ fov: 45 }}
+      style={{ position: "absolute" }}
+    >
+      <color attach="background" args={["#808b3e"]} />
+      <PresentationControls
+        speed={1.5}
+        global
+        zoom={0.7}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage environment={null}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
+  );
 }
+
+export default Dress1
