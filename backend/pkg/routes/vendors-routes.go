d2782e8b6495fdcb873f046f3b6a8fc158f777a8
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-12 13:16:35 +0300

    backend: creating uploading image route

--- backend/pkg/routes/vendors-routes.go
+++ backend/pkg/routes/vendors-routes.go
@@ -7,9 +7,21 @@ import (
 
 var RegisterVendorsRoutes = func(router *mux.Router) {
 	router.HandleFunc("/register/vendor", controllers.CreateVendor).Methods("POST")
+	router.HandleFunc("/upload-image", controllers.UploadImage).Methods("POST")
+
 	router.HandleFunc("/vendor", controllers.GetAllVendors).Methods("GET")
 	router.HandleFunc("/vendor/{vendorID}", controllers.GetVendorByID).Methods("GET")
 	router.HandleFunc("/vendor/{vendorID}", controllers.DeleteVendor).Methods("DELETE")
 	router.HandleFunc("/vendor/{vendorID}", controllers.UpdateVendor).Methods("PUT")
 	router.HandleFunc("/vendors", controllers.GetVendorsByCategory).Methods("POST")
 }
+
+// {
+// 	"companyName" :"company_name",
+// "phone"         :"phone",
+// "email"         :"email",
+// "socialMedia"   :"social_media",
+// "facilities"    :["facilities"],
+// "vision"        :"vision",
+// "category"      :"cars"
+// }
