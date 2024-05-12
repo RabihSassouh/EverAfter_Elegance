63ccc275af8fa9f96606ab9e88f0e080467f0254
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 02:46:32 +0300

    frontend: adding title to form

--- frontend/src/components/SignupStep1.tsx
+++ frontend/src/components/SignupStep1.tsx
@@ -5,6 +5,8 @@ const Step1 = () => {
 
   return (
     <div className='w-full h-screen flex items-center justify-center'>
+            <h1 className="text-4xl font-semibold font-poppins mb-10 text-center">Welcome User</h1>
+
         <div className='absolute w-[55%] h-full right-0 hidden md:block'>
           <div className='relative h-full w-full'>
             <div className="absolute inset-0 bg-[#00000066] rounded-xl opacity-70"></div>
@@ -12,8 +14,8 @@ const Step1 = () => {
             <button className="absolute bottom-5 right-5 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors">Next</button>
           </div>
         </div>
-      
-    </div>
+      </div>
+
   )
 }
 
