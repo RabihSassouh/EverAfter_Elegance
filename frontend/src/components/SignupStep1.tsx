d112c5420753331d7c15bfc7b58107d2031b03a0
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 02:42:58 +0300

    frontend: creating next button in signupStep1

--- frontend/src/components/SignupStep1.tsx
+++ frontend/src/components/SignupStep1.tsx
@@ -5,8 +5,11 @@ const Step1 = () => {
 
   return (
     <div className='w-full h-screen flex items-center justify-center'>
-      <img src="../../signUp2.png" alt="Couple Image" className="object-cover h-full w-full rounded-xl"/>
-            
+            <div className="absolute inset-0 bg-[#00000066] rounded-xl opacity-70"></div>
+            <img src="../../signUp2.png" alt="Couple Image" className="object-cover h-full w-full rounded-xl"/>
+            <button className="absolute bottom-5 right-5 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors">Next</button>
+          
+
     </div>
   )
 }
