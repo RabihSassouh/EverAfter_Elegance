b74bcc0786a3272297bd6b2a021e78757333966d
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 21:53:45 +0300

    frontend: adding title to couples information

--- frontend/src/components/CoupleInfo.tsx
+++ frontend/src/components/CoupleInfo.tsx
@@ -3,7 +3,12 @@
 const CouplesInfo = () => {
 
     return (
-        <div>
+        <div className="min-h-screen flex items-center justify-center  px-12 bg-white">
+                <div className='mb-16 flex flex-col gap-8'>
+                    <h1 className="text-4xl font-semibold font-poppins text-[#000000CC] text-center">
+                        Couples information
+                    </h1>
+                </div>
         </div>
     )
 }
