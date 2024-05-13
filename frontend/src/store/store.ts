69658c8124e2a632373a05f5bc1a50061f468c46
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 11:56:52 +0300

    frontend: adding rootReducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,6 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore } from '@reduxjs/toolkit';
 import utilityReducer from '../reducers/utilitySlice';
 import signUpReducer from './signUpSlice';
+
+
+const rootReducer = combineReducers({
+    utility: utilityReducer,
+    signUp: signUpReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export const store = configureStore({
     reducer: {
