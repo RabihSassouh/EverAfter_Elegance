9a674fe0efed4b7144d6db99d9f70984e47bea9d
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 02:12:03 +0300

    frontend: creating Business Category

--- frontend/src/components/BusinessCategory.tsx
+++ frontend/src/components/BusinessCategory.tsx
@@ -45,7 +45,26 @@ const BusinessCategory: React.FC = () => {
     };
 
     return (
-        <div></div>
+        <div className='w-full min-h-screen flex items-center justify-center'>
+            <div className="my-12 px-12 text-center h-full flex flex-col gap-5 items-center justify-center">
+                <h1 className="text-4xl font-medium font-poppins text-[#000000CC]">Which of these best describes your business?</h1>
+                <div className="mt-5 grid grid-cols-2 md:grid-cols-4 gap-6">
+                    {categories.map((category, index) => (
+                        <div key={index} className={`flex flex-col items-center justify-center bg-white rounded-xl cursor-pointer group`} onClick={() => handleCategorySelect(category)}>
+                            <div className='relative w-full h-32 mb-2'>
+                                <div className="absolute inset-0 bg-[#00000066] rounded-xl opacity-70"></div>
+                                <img src={category.image} alt={category.name} className={`w-full h-32 object-cover rounded-xl mb-2 border-[2px] border-[#00000033] group-hover:border-primary ${data?.category?.id === category.id ? 'border-primary' : ''}`} />
+                            </div>
+                            <span className={`text-center text-[#00000099] font-poppins font-semibold group-hover:text-primary ${data?.category?.id === category.id ? 'text-primary' : ''}`}>{category.name}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className='flex w-full items-end justify-end'>
+                    <button className="bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors" onClick={handleNext}>Next</button>
+                </div>
+            </div>
+
+        </div>
     );
 };
 
