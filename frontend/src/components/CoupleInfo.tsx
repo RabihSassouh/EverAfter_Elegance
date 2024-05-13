328b0cc2b7e5273c4c8de173c5f43008cd7d4a53
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 21:56:29 +0300

    frontend: adding bride from

--- frontend/src/components/CoupleInfo.tsx
+++ frontend/src/components/CoupleInfo.tsx
@@ -10,12 +10,24 @@ const CouplesInfo = () => {
                     </h1>
                 </div>
                 <div className='flex flex-col md:flex-row gap-20 mb-12 w-full'>
+                    <div className="w-full md:w-1/2 h-full md:p-14 md:shadow-xl">
+                        <div className='mb-8 flex flex-col items-center justify-center'>
                             <h2 className='font-poppins text-4xl font-semibold text-center text-[#000000CC] tracking-wide'>
                                 Bride
                             </h2>
                             <hr className='border-[2px] border-primary w-32'/>
                         </div>
-                     
+                        <div className='flex flex-col gap-4 h-full items-center justify-between w-full'>
+                            <form className='h-full flex flex-col gap-4 w-full' >
+                                <input type="text" className='w-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='First name' name='first_name'/>
+                                <input type="text" className='w-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Last name' name='last_name'/>
+                                <input type="text" className='w-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Email' name='email'/>
+                                <input type="text" className='w-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Phone' name='phone'/>
+                            </form>
+                        </div>
+                    </div>
+                    
+                </div>
         </div>
     )
 }
