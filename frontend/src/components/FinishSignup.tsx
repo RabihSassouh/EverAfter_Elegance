fa0b09621eef7bd795db8891dcb7be80598ece46
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 01:44:54 +0300

    frontend: adding Welcoming form in Finish sign up.

--- frontend/src/components/FinishSignup.tsx
+++ frontend/src/components/FinishSignup.tsx
@@ -2,7 +2,15 @@ const FinishSignup = () => {
     const userType = 1;
   return (
     <div className='w-full mt-20 flex flex-col gap-5 items-center justify-center' style={{height:450}}>
-          <h1 className="text-4xl font-semibold font-poppins mb-10 text-center">Welcome to EverAfter Elegance</h1>
+      <div className='md:w-[45%] max-w-lg mx-5 h-full z-10 flex items-center justify-center border-[1px] border-primary rounded-xl'>
+          <div className='bg-white h-[80%] p-10 rounded-xl flex flex-col items-center justify-center'>
+            <h1 className="text-4xl font-semibold font-poppins mb-10 text-center">Welcome to EverAfter Elegance</h1>
+            <p className='font-poppins font-medium text-center mb-8'>
+              {userType === 1 ? "We are here to make your dream day a reality..." : "It is our honor to have you as a part of our community..."}
+            </p>
+            <p className='text-xl font-poppins font-semibold mb-3 text-center'>Finish up and publish</p>
+          </div>
+        </div>
         
     </div>
   );
