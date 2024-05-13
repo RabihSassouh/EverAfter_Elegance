cbbc2854e77acd8c76dc837f2241478de5206a1b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-13 21:57:46 +0300

    frontend: setting bride info form and title

--- frontend/src/components/CoupleInfo.tsx
+++ frontend/src/components/CoupleInfo.tsx
@@ -4,6 +4,7 @@ const CouplesInfo = () => {
 
     return (
         <div className="min-h-screen flex items-center justify-center  px-12 bg-white">
+            <div className="container max-w-5xl mx-auto">
                 <div className='mb-16 flex flex-col gap-8'>
                     <h1 className="text-4xl font-semibold font-poppins text-[#000000CC] text-center">
                         Couples information
@@ -28,6 +29,8 @@ const CouplesInfo = () => {
                     </div>
                     
                 </div>
+                
+            </div>
         </div>
     )
 }
