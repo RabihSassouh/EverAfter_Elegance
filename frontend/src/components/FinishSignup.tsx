cfe1a9917a2c615640392b72051de08c3363d9c0
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 01:45:48 +0300

    frontend: adding image to finish signup

--- frontend/src/components/FinishSignup.tsx
+++ frontend/src/components/FinishSignup.tsx
@@ -2,7 +2,8 @@ const FinishSignup = () => {
     const userType = 1;
   return (
     <div className='w-full mt-20 flex flex-col gap-5 items-center justify-center' style={{height:450}}>
-      <div className='md:w-[45%] max-w-lg mx-5 h-full z-10 flex items-center justify-center border-[1px] border-primary rounded-xl'>
+      <div className='relative flex items-center justify-center md:justify-between mx-12 max-w-5xl h-450 w-full bg-white'>
+        <div className='md:w-[45%] max-w-lg mx-5 h-full z-10 flex items-center justify-center border-[1px] border-primary rounded-xl'>
           <div className='bg-white h-[80%] p-10 rounded-xl flex flex-col items-center justify-center'>
             <h1 className="text-4xl font-semibold font-poppins mb-10 text-center">Welcome to EverAfter Elegance</h1>
             <p className='font-poppins font-medium text-center mb-8'>
@@ -11,7 +12,13 @@ const FinishSignup = () => {
             <p className='text-xl font-poppins font-semibold mb-3 text-center'>Finish up and publish</p>
           </div>
         </div>
-        
+        <div className='w-[45%] h-450 right-0 hidden md:block'>
+          <div className='relative h-full w-full'>
+            <div className="absolute inset-0 bg-[#00000066] rounded-xl opacity-70"></div>
+            <img src="../../signUp2.png" alt="Couple Image" className="object-cover h-450 w-full rounded-xl" style={{height:450}}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
