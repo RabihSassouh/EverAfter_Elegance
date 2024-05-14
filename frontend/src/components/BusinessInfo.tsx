9708945ecdbfe9800e12ae7ff506edc5ccabd170
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 11:29:22 +0300

    frontend: creating handle next func

--- frontend/src/components/BusinessInfo.tsx
+++ frontend/src/components/BusinessInfo.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { setData, setStep } from '../store/signUpSlice';
+import BusinessInformationSchema from '../validationSchemas/BusinessInfoSchema';
+
+interface BusinessInformationFormData {
+    description: string;
+    company_name: string;
+    phone_number: string;
+    email: string;
+    social_media: string;
+    facilities: string;
+    vision: string;
+}
 
 const BusinessInformation: React.FC = () => {
+    const dispatch = useDispatch();
+    const businessInfoForm = useRef<HTMLFormElement>(null);
+
+    const handleNext = async () => {
+        if (!businessInfoForm.current) return;
+
+        const businessInfoFormData = new FormData(businessInfoForm.current);
+        try {
+            const business_information: Partial<BusinessInformationFormData> = {};
+            for (let [key, value] of businessInfoFormData.entries()) {
+                business_information[key as keyof BusinessInformationFormData] = value as string;
+            }
+            await BusinessInformationSchema.validate(business_information, { abortEarly: false });
+
+            // If validation succeeds, continue with form submission
+            const modData = {
+                business_information
+            };
+            dispatch(setData(modData));
+            dispatch(setStep(8));
+        } catch (error: any) {
+            toast.error(`${error.message}`);
+            console.error('Validation errors:', error.message);
+        }
+    };
 
     return (
         <div className="min-h-screen flex items-center justify-center py-12 px-12 bg-white">
             <div className="container max-w-4xl mx-auto">
-                <form action="" >
+                <form action="" ref={businessInfoForm}>
                     <div className='mb-16 flex flex-col gap-8'>
                         <h1 className="text-3xl font-medium font-poppins text-[#000000CC] text-center">
                             Tell guests what you place has to offer
