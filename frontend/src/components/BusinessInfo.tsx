db94578245fff72b6231d09ca5dd4b6ce7f98ba0
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 11:21:44 +0300

    frontend: adding details to form

--- frontend/src/components/BusinessInfo.tsx
+++ frontend/src/components/BusinessInfo.tsx
@@ -16,6 +16,16 @@ const BusinessInformation: React.FC = () => {
                         <h1 className='text-3xl font-medium font-poppins text-[#000000CC] text-center'>
                             Your business information
                         </h1>
+                        <div className='flex flex-col md:h-52 md:flex-row gap-8 items-center justify-between'>
+                            <div className='flex flex-col gap-4 h-full items-center justify-between'>
+                                <input name='company_name' type="text" className='border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Company name'/>
+                                <input name='phone_number' type="text" className='border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Phone number'/>
+                                <input name='email' type="text" className='border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Email'/>
+                                <input name='social_media' type="text" className='border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins' placeholder='Social media'/>
+                            </div>
+                            <textarea name="facilities" id="" className='w-full md:w-auto h-28 md:h-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins placeholder:text-center' placeholder='Facilities'></textarea>
+                            <textarea name="vision" id="" className='w-full md:w-auto h-28 md:h-full border-[1px] border-gray-600 rounded-xl font-poppins px-4 py-2 placeholder:font-poppins placeholder:text-center' placeholder='Vision'></textarea>
+                        </div>
                     </div>
                 </form>
                 
