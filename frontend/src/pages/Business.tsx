2d775fed0f1be34ae8649114661d258c089b5686
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-15 01:56:51 +0300

    frontend: creating upload image

--- frontend/src/pages/Business.tsx
+++ frontend/src/pages/Business.tsx
@@ -1,8 +1,39 @@
 import React, { useEffect, useRef, useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
+import { CiImageOn } from "react-icons/ci";
+import { BsUpload } from "react-icons/bs";
+import { FaTimes } from "react-icons/fa";
 
 const Business: React.FC = () => {
+  const photoInputRef = useRef<HTMLInputElement>(null);
+  const [photos, setPhotos] = useState<File[]>([]);
+
+  // Function to handle adding photos
+  const handleAddPhoto = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const updatedPhotos = [...photos];
+      for (let i = 0; i < files.length; i++) {
+        updatedPhotos.push(files[i]);
+      }
+      setPhotos(updatedPhotos);
+    }
+  };
+
+
+
+  // Function to handle removing photos
+  const handleRemovePhoto = (index: number) => {
+    const updatedPhotos = [...photos];
+    updatedPhotos.splice(index, 1);
+    setPhotos(updatedPhotos);
+  };
+
+
+  const handleDivClick = (inputRef: React.RefObject<HTMLInputElement>) => {
+    inputRef.current?.click();
+  };
 
   useEffect(() => {
     document.title = "Your Business";
@@ -132,7 +163,56 @@ const Business: React.FC = () => {
                 </div>
               </div>
             </div>
-
+            <div className="flex flex-col gap-1">
+              <label htmlFor="" className="font-poppins text-[#494949]">
+                Please Upload Some Images And Videos Of Your Work
+              </label>
+              <div className="flex flex-wrap gap-8">
+                {photos.map((photo, i) => {
+                  return (
+                    <div key={i} className="flex flex-col gap-2 cursor-pointer">
+                      <div className="border-[2px] border-[#00000033] border-dashed h-32 w-44 rounded-xl flex items-center justify-center">
+                        <div className="relative h-32 w-44">
+                          <FaTimes
+                            className="z-20 absolute top-0 right-0 cursor-pointer text-red-500 bg-white text-xl rounded-full"
+                            onClick={() => handleRemovePhoto(i)}
+                          />
+                          <img
+                            src={URL.createObjectURL(photo)}
+                            alt={`Photo ${i}`}
+                            className="w-full object-cover rounded-xl cursor-pointer h-full"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+                
+                <div
+                  className="flex flex-col gap-2 cursor-pointer"
+                  onClick={() => handleDivClick(photoInputRef)}
+                >
+                  <div className="border-[2px] border-[#00000033] border-dashed h-32 w-44 rounded-xl flex items-center justify-center">
+                    <input
+                      ref={photoInputRef}
+                      type="file"
+                      id="add_photo"
+                      accept="image/*"
+                      multiple
+                      style={{ display: "none" }}
+                      onChange={handleAddPhoto}
+                    />
+                    <CiImageOn className="text-6xl text-[#00000066]" />
+                  </div>
+                  <button className=" font-poppins bg-primary text-white px-4 py-2 rounded-xl flex items-center gap-3">
+                    Upload Image
+                    <BsUpload />
+                  </button>
+                </div>
+                
+              </div>
+            </div>
+       
           </div>
         </div>
       </div>
