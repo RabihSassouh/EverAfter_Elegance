aa8a1e176ee67e7d97e108a44b0820d4a6d5b16c
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 22:07:34 +0300

    frontend: adding wedding information

--- frontend/src/pages/Couple.tsx
+++ frontend/src/pages/Couple.tsx
@@ -13,7 +13,12 @@ const Couples: React.FC = () => {
           <div className="container mx-auto text-center mb-8">
             <h2 className="text-4xl font-semibold font-poppins mb-5 text-[#121212]">Make Your Wedding Invitation</h2>
           </div>
-         </div> 
+          <div className="container mx-auto w-full flex flex-col gap-6 items-start justify-center">
+            <h4 className="text-2xl font-medium font-poppins text-primary">Wedding Information</h4>
+           
+              </div>
+            </div>
+           
       </div>
       <Footer />
     </div>
