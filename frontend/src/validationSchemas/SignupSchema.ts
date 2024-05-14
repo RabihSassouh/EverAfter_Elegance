dd8c63c015451a2e5e8c5a6b8db12815fa9b1027
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-15 02:23:51 +0300

    frontend: adding signup schema for validation

---
+++ frontend/src/validationSchemas/SignupSchema.ts
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+
+export const SignupSchema = Yup.object().shape({
+    firstname: Yup.string().required('First Name is required'),
+    lastname: Yup.string().required('Last Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .required('Password is required'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], 'Passwords must match')
+        .required('Confirm Password is required'),
+});
