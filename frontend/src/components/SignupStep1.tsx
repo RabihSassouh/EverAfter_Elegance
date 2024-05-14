8b5b5b6c419d4adf70774e9301bc77192facb10f
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 20:35:19 +0300

    frontend: fixing the handle next funct in step 1 in signup form

--- frontend/src/components/SignupStep1.tsx
+++ frontend/src/components/SignupStep1.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { setStep, setUserType } from "../store/signUpSlice";
-import { RootState } from "../store/store";
-
 const Step2: React.FC = () => {
   const dispatch = useDispatch();
-  const { userType } = useSelector((state: RootState) => state.signUp);
+  const { userType } = useSelector((state: any) => state.signUp);
 
   const handleUserType = (value: number) => {
     dispatch(setUserType(value));
@@ -17,6 +15,7 @@ const Step2: React.FC = () => {
   const handleNext = () => {
     if (userType === null) {
       toast.error(`Select a user type`);
+      return;
     } else {
       dispatch(setStep(3));
     }
@@ -28,7 +27,7 @@ const Step2: React.FC = () => {
         <div className="absolute w-[80%] md:w-[45%] h-full md:left-20 z-10 flex items-center justify-center">
           <div className="bg-white h-[80%] md:p-10 rounded-xl flex flex-col items-center justify-center">
             <h1 className="text-4xl font-semibold font-poppins mb-10 text-center">
-              Welcome User
+              Welcome
             </h1>
             <p className="text-2xl font-poppins font-semibold mb-1 text-center">
               Step 1
@@ -41,19 +40,40 @@ const Step2: React.FC = () => {
               with and ask you about some information accordingly.
             </p>
             <div className="flex flex-col w-full">
-              <button
-                className={`mb-4 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded hover:shadow-md transition-colors
-                ${userType === 1 ? 'border-[2px] border-gray-300 bg-transparent text-gray-700' : 'hover:bg-secondary'}`} onClick={() => handleUserType(1)}
-              >
-                Couple
-              </button>
-              <button
-                className={`mb-4 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded hover:shadow-md transition-colors
-                ${userType === 2 ? 'border-[2px] border-gray-300 bg-transparent text-gray-700' : 'hover:bg-secondary'}`} onClick={() => handleUserType(2)}
-              >
-                Business owner
-              </button>
+              <p className="font-poppins font-medium text-center mb-8">
+                {userType === 1
+                  ? "We are here to make your dream day a reality..."
+                  : "It is our honor to have you as a part of our community..."}
+              </p>
+              <div className="flex flex-col w-full">
+                <button
+                  className={`mb-4 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded hover:shadow-md transition-colors ${
+                    userType === 1
+                      ? "border-[2px] border-gray-300 bg-transparent text-gray-700"
+                      : "hover:bg-secondary"
+                  }`}
+                  onClick={() => handleUserType(1)}
+                >
+                  Couple
+                </button>
+                <button
+                  className={`mb-4 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded hover:shadow-md transition-colors ${
+                    userType === 2
+                      ? "border-[2px] border-gray-300 bg-transparent text-gray-700"
+                      : "hover:bg-secondary"
+                  }`}
+                  onClick={() => handleUserType(2)}
+                >
+                  Business owner
+                </button>
+              </div>
             </div>
+            <button
+              className="absolute md:hidden bottom-0 right-0 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors"
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
         <div className="absolute w-[55%] h-full right-0 hidden md:block">
@@ -64,8 +84,9 @@ const Step2: React.FC = () => {
               alt="Couple Image"
               className="object-cover h-full w-full rounded-xl"
             />
-            <button className="absolute bottom-5 right-5 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors"
-            onClick={handleNext}
+            <button
+              className="absolute bottom-5 right-5 bg-primary  font-poppins text-[#FFFFFFEB] font-semibold px-6 py-2 rounded-full hover:bg-secondary hover:shadow-md transition-colors"
+              onClick={handleNext}
             >
               Next
             </button>
