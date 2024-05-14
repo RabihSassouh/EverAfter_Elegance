d072d10aa1f3999157da0568a4f55ff2c842eee3
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 10:59:56 +0300

    frontend: adding Add photo button

--- frontend/src/components/BusinessPhotos.tsx
+++ frontend/src/components/BusinessPhotos.tsx
@@ -7,6 +7,14 @@ const BusinessPhotos: React.FC = () => {
         <div className='w-full min-h-screen flex items-center justify-center'>
             <div className="my-12 px-12 text-center h-full flex flex-col gap-5 items-center justify-center">
                 <h1 className="text-4xl font-medium font-poppins text-[#000000CC]">Choose photos for your business</h1>
+                <div className='flex w-full items-end justify-center mt-5'>
+                    <button className="bg-white flex items-center gap-3 font-poppins text-[#000000CC] border-[2px] border-gray-600 font-semibold px-6 py-2 rounded-xl hover:shadow-md transition-colors" onClick={handlePhotos}>
+                        <FaPlus />
+                        Add photos
+                    </button>
+                    <input id="photoInput" type="file" style={{ display: 'none' }} onChange={handlePhotoInputChange} multiple />
+                </div>
+                
             </div>
         </div>
     )
