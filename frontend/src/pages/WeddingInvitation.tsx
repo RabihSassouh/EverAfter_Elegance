a103be60acbcb4b656b294e3ac74f87fbf9f5662
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-15 01:08:50 +0300

    frontend: adding invitation quote and title

--- frontend/src/pages/WeddingInvitation.tsx
+++ frontend/src/pages/WeddingInvitation.tsx
@@ -26,7 +26,18 @@ const WeddingInvitation: React.FC = () => {
           </p>
         </div>
       </div>
-      
+      <div className="container mx-auto flex flex-col items-center justify-center px-12 py-12">
+        <div className="text-center mb-8">
+          <h2 className="text-4xl font-semibold font-poppins mb-5 text-[#121212] tracking-wide">
+            Invitation For The Wedding
+          </h2>
+          <p className="font-poppins text-[#494949]">
+            We're thrilled to see you here! Get ready for a cocktail of love,
+            laughter, and happily-ever-afters. See you at the wedding!
+          </p>
+        </div>
+        
+      </div>
       <Footer />
     </div>
   );
