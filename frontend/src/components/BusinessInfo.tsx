a15dfe50e3942cdeb2810917bb92a6608fbd6341
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-14 11:19:58 +0300

    frontend: add businessInfo component and its title

---
+++ frontend/src/components/BusinessInfo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BusinessInformation: React.FC = () => {
+
+    return (
+        <div className="min-h-screen flex items-center justify-center py-12 px-12 bg-white">
+            <div className="container max-w-4xl mx-auto">
+                    <div className='mb-16 flex flex-col gap-8'>
+                        <h1 className="text-3xl font-medium font-poppins text-[#000000CC] text-center">
+                            Tell guests what you place has to offer
+                        </h1>
+                        <textarea name="description" id="" className='border-[1px] border-gray-600 w-full rounded-xl h-44 font-poppins px-4 py-2'></textarea>
+                    </div>
+               
+            </div>
+        </div>
+    );
+};
+
+export default BusinessInformation;
