83e9cd1aa408d3e7e33e7de4c0047c1f7eef583b
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-15 16:21:17 +0300

    frontend: adding toastContainer to handle errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,8 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import './scrollBar.css'
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 const root = ReactDOM as any;
@@ -13,6 +14,18 @@ root.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        bodyClassName={'text-gray-600 font-poppins'}
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Provider>
   </React.StrictMode>
 );
