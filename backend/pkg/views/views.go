d2753fda4d7fa5084f97b16902a64ed0d67fc103
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-17 01:39:19 +0300

    backend: adding stripe.go views

---
+++ backend/pkg/views/views.go
@@ -0,0 +1,14 @@
+// views.go
+
+package views
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RenderJSON(w http.ResponseWriter, data interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
