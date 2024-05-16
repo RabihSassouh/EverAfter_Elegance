6670f464e9cf3b34b9951673a9d3b763d25ff71e
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-16 11:39:07 +0300

    backend: adding Authorization to CORS

--- backend/pkg/middleware/cors-middleware.go
+++ backend/pkg/middleware/cors-middleware.go
@@ -8,7 +8,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
         if r.Method == http.MethodOptions {
             return
