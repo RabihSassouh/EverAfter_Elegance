9702a01dc902e7cf49d211add46003fbca436f40
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-16 04:23:46 +0300

    backend: creating getUserData func

--- backend/pkg/controllers/users-controller.go
+++ backend/pkg/controllers/users-controller.go
@@ -3,17 +3,100 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+
+	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/middleware"
 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/models"
 	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/utils"
-	"github.com/RabihSassouh/EverAfter_Elegance/backend/pkg/middleware"
+
 	// "github.com/dgrijalva/jwt-go"
-	"github.com/gorilla/mux"
-	"golang.org/x/crypto/bcrypt"
+	"errors"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var NewUser models.User
+
+var store = sessions.NewCookieStore([]byte("your-secret-key"))
+type Response struct {
+	Message string `json:"message"`
+}
+
+func ValidateEmailHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser models.User
+	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
+		fmt.Println("Error decoding request body:", err)
+		respondWithError(w, map[string]string{"error": "Invalid request"}, http.StatusBadRequest)
+		return
+	}
+
+	if err := models.ValidateEmail(newUser.Email); err != nil {
+		fmt.Println("Email validation error:", err)
+		respondWithError(w, map[string]string{"error": err.Error()}, http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("Error hashing password:", err)
+		respondWithError(w, map[string]string{"error": "Failed to hash password"}, http.StatusInternalServerError)
+		return
+	}
+
+	session, err := store.Get(r, "user-session")
+	if err != nil {
+		fmt.Println("Error getting user session:", err)
+		respondWithError(w, map[string]string{"error": "Failed to create session"}, http.StatusInternalServerError)
+		return
+	}
+
+	session.Values["firstname"] = newUser.FirstName
+	session.Values["lastname"] = newUser.LastName
+	session.Values["email"] = newUser.Email
+	session.Values["password"] = hashedPassword
+
+	fmt.Println("Session Data:")
+	fmt.Println("Firstname:", session.Values["firstname"])
+	fmt.Printf("Type of firstname: %T\n", "Firstname")
+
+	fmt.Println("  Lastname:", session.Values["lastname"])
+	fmt.Println("  Email:", session.Values["email"])
+
+	defer session.Save(r, w)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+
+
+func GetUserDataFromSession(r *http.Request) (firstname, lastname, email, password string, err error) {
+	session, err := store.Get(r, "user-session")
+	if err != nil {
+		fmt.Println("Error getting user session:", err)
+		return "", "", "", "", errors.New("failed to get user session")
+	}
+	// Print session values for debugging
+	fmt.Println("Session Values:")
+	fmt.Println("  Firstname:", session.Values["firstname"])
+	fmt.Println("  Lastname:", session.Values["lastname"])
+	fmt.Println("  Email:", session.Values["email"])
+	fmt.Println("  Password:", session.Values["password"])
+
+	if session.Values["firstname"] == nil || session.Values["lastname"] == nil || session.Values["email"] == nil || session.Values["password"] == nil {
+		fmt.Printf("Session values missing")
+		return "", "", "", "", errors.New("user data not found in session")
+	}
+
+	firstname = session.Values["firstname"].(string)
+	lastname = session.Values["lastname"].(string)
+	email = session.Values["email"].(string)
+	password = session.Values["password"].(string)
+
+	return firstname, lastname, email, password, nil
+}
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	newUser := &models.User{}
@@ -78,43 +161,42 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	createdUser := newUser.CreateUser()
-    if createdUser == nil {
-        errorMessage := map[string]string{"error": "failed to create user"}
-        respondWithError(w, errorMessage, http.StatusInternalServerError)
-        return
-    }
-	
+	if createdUser == nil {
+		errorMessage := map[string]string{"error": "failed to create user"}
+		respondWithError(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+
 	if newUser.UserType == "vendor" {
-        res, _ := json.Marshal(map[string]string{"message": "Vendor registration requires additional information"})
-        w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusOK)
-        w.Write(res)
-        return
-    }
+		res, _ := json.Marshal(map[string]string{"message": "Vendor registration requires additional information"})
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(res)
+		return
+	}
 
+	// Create the user
+	// createdUser := newUser.CreateUser()
+	// if createdUser == nil {
+	//     errorMessage := map[string]string{"error": "failed to create user"}
+	//     res, _ := json.Marshal(errorMessage)
+	//     w.Header().Set("Content-Type", "application/json")
+	//     w.WriteHeader(http.StatusInternalServerError)
+	//     w.Write(res)
+	//     return
+	// }
 
-// Create the user
-// createdUser := newUser.CreateUser()
-// if createdUser == nil {
-//     errorMessage := map[string]string{"error": "failed to create user"}
-//     res, _ := json.Marshal(errorMessage)
-//     w.Header().Set("Content-Type", "application/json")
-//     w.WriteHeader(http.StatusInternalServerError)
-//     w.Write(res)
-//     return
-// }
-
-res, _ := json.Marshal(createdUser)
-w.Header().Set("Content-Type", "application/json")
-w.WriteHeader(http.StatusOK)
-w.Write(res)
+	res, _ := json.Marshal(createdUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func respondWithError(w http.ResponseWriter, errorMessage map[string]string, statusCode int) {
-    res, _ := json.Marshal(errorMessage)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(statusCode)
-    w.Write(res)
+	res, _ := json.Marshal(errorMessage)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(res)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -169,23 +251,23 @@ func GetUserById(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tokenString, err := utils.GenerateToken(user, middleware.JWTKey)
-    if err != nil {
-        http.Error(w, "Failed to generate JWT token", http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, "Failed to generate JWT token", http.StatusInternalServerError)
+		return
+	}
 
 	// Return user details (excluding password and ID) and JWT token
 	userDetails := struct {
 		FirstName string `json:"firstname"`
 		LastName  string `json:"lastname"`
 		Email     string `json:"email"`
-		UserType string `json:"user_type"`
+		UserType  string `json:"user_type"`
 		Token     string `json:"token"`
 	}{
 		FirstName: user.FirstName,
 		LastName:  user.LastName,
 		Email:     user.Email,
-		UserType: user.UserType,
+		UserType:  user.UserType,
 		Token:     tokenString,
 	}
 
@@ -234,7 +316,7 @@ func UpdateUser(w http.ResponseWriter, r *http.Request) {
 	if updateUser.Email != "" {
 		userDetails.Email = updateUser.Email
 	}
-	if updateUser.Password != ""  {
+	if updateUser.Password != "" {
 		// Update the password only if a new password is provided
 		hashedPassword, err := models.HashPassword(updateUser.Password)
 		if err != nil {
