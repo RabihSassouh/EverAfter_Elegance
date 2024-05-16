0d75adda181024c45b20b5f09f4f4b32bf8576c4
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-16 22:27:45 +0300

    frontend: navigating to wedding invitation page when publishing invitation

--- frontend/src/pages/Couple.tsx
+++ frontend/src/pages/Couple.tsx
@@ -3,6 +3,7 @@ import NavigationBar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import { MdPublish } from "react-icons/md";
 import { BsFillGiftFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 interface Gift {
   id: number;
@@ -28,20 +29,21 @@ interface Info {
 const Couples: React.FC = () => {
   const giftInputRef = useRef<HTMLInputElement>(null);
   const [gifts, setGifts] = useState<Gift[]>([]);
-  const user = localStorage.getItem('id')
+  const user = localStorage.getItem("id");
+  const navigate = useNavigate();
   const [info, setInfo] = useState<Info>({
-    groom_firstname: '',
-    groom_lastname: '',
-    groom_email: '',
-    groom_phone: '',
-    bride_firstname: '',
-    bride_lastname: '',
-    bride_email: '',
-    bride_phone: '',
-    wedding_date: '',
-    venue_preference: '',
-    budget: '',
-    guest_count: '',
+    groom_firstname: "",
+    groom_lastname: "",
+    groom_email: "",
+    groom_phone: "",
+    bride_firstname: "",
+    bride_lastname: "",
+    bride_email: "",
+    bride_phone: "",
+    wedding_date: "",
+    venue_preference: "",
+    budget: "",
+    guest_count: "",
   });
 
   const brideDress = [1];
@@ -50,15 +52,17 @@ const Couples: React.FC = () => {
     // Function to fetch couple data from backend
     const fetchCouple = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8080/get-couple/${user}`);
+        const response = await fetch(
+          `http://127.0.0.1:8080/get-couple/${user}`
+        );
         if (!response.ok) {
-          throw new Error('Failed to fetch couple');
+          throw new Error("Failed to fetch couple");
         }
         const infoData = await response.json();
         setInfo(infoData);
-        console.log(infoData)
+        console.log(infoData);
       } catch (error) {
-        console.error('Error fetching couple:', error);
+        console.error("Error fetching couple:", error);
       }
     }; // Call the fetchCouple function
     fetchCouple();
@@ -80,15 +84,19 @@ const Couples: React.FC = () => {
     }
   };
 
-  const handleGiftValueChange = (index: number, value: number) => {
-    const updatedGifts = [...gifts];
-    updatedGifts[index].value = value;
-    setGifts(updatedGifts);
-  };
+  // const handleGiftValueChange = (index: number, value: number) => {
+  //   const updatedGifts = [...gifts];
+  //   updatedGifts[index].value = value;
+  //   setGifts(updatedGifts);
+  // };
 
   const handleRemoveGift = (index: number) => {
     const updatedGifts = gifts.filter((_, i) => i !== index);
     setGifts(updatedGifts);
+  };
+
+  const handlePublish = () => {
+    navigate("/wedding-invitation");
   };
 
   return (
@@ -126,8 +134,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter bride's first name"
                       value={info.bride_firstname}
-                      onChange={(e) => setInfo({ ...info, bride_firstname: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, bride_firstname: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -144,8 +153,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter bride's last name"
                       value={info.bride_lastname}
-                      onChange={(e) => setInfo({ ...info, bride_lastname: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, bride_lastname: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -162,8 +172,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter bride's email"
                       value={info.bride_email}
-                      onChange={(e) => setInfo({ ...info, bride_email: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, bride_email: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -180,8 +191,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter bride's phone number"
                       value={info.bride_phone}
-                      onChange={(e) => setInfo({ ...info, bride_phone: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, bride_phone: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -229,7 +241,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter groom's first name"
                       value={info.groom_firstname}
-                      onChange={(e) => setInfo({ ...info, groom_firstname: e.target.value })}
+                      onChange={(e) =>
+                        setInfo({ ...info, groom_firstname: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -246,7 +260,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter groom's last name"
                       value={info.groom_lastname}
-                      onChange={(e) => setInfo({ ...info, groom_lastname: e.target.value })}
+                      onChange={(e) =>
+                        setInfo({ ...info, groom_lastname: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -263,8 +279,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter groom's email"
                       value={info.groom_email}
-                      onChange={(e) => setInfo({ ...info, groom_email: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, groom_email: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -281,7 +298,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter groom's phone number"
                       value={info.groom_phone}
-                      onChange={(e) => setInfo({ ...info, groom_phone: e.target.value })}
+                      onChange={(e) =>
+                        setInfo({ ...info, groom_phone: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -331,8 +350,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter venue preference"
                       value={info.venue_preference}
-                      onChange={(e) => setInfo({ ...info, venue_preference: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, venue_preference: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -349,8 +369,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter wedding date"
                       value={info.wedding_date}
-                      onChange={(e) => setInfo({ ...info, wedding_date: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, wedding_date: e.target.value })
+                      }
                     />
                   </div>
                 </div>
@@ -369,8 +390,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter wedding budget"
                       value={info.budget}
-                      onChange={(e) => setInfo({ ...info, budget: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, budget: e.target.value })
+                      }
                     />
                   </div>
                   <div className="flex flex-col gap-1">
@@ -387,8 +409,9 @@ const Couples: React.FC = () => {
                       className="border-[2px] border-[#00000033] rounded-xl py-2 px-4 text-[#00000066] placeholder:text-[#00000066] font-poppins"
                       placeholder="Please enter wedding guests count"
                       value={info.guest_count}
-                      onChange={(e) => setInfo({ ...info, guest_count: e.target.value })}
-                 
+                      onChange={(e) =>
+                        setInfo({ ...info, guest_count: e.target.value })
+                      }
                     />
                   </div>
                 </div>
@@ -447,7 +470,10 @@ const Couples: React.FC = () => {
               </div>
             </div>
             <div className="flex items-center justify-end w-full gap-5 font-poppins mt-5">
-              <button className="flex items-center gap-1 border border-primary bg-primary text-white rounded-xl px-4 py-2 text-[#00000066] hover:shadow-md hover:bg-secondary">
+              <button
+                className="flex items-center gap-1 border border-primary bg-primary text-white rounded-xl px-4 py-2 text-[#00000066] hover:shadow-md hover:bg-secondary"
+                onClick={handlePublish}
+              >
                 Publish Invitation
                 <MdPublish fontSize={20} />
               </button>
