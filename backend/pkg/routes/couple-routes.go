7b6c2b5b35026a2222c2d761ecb8e55f3105543a
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-05-16 21:19:56 +0300

    backend: adding getCouplesById route

--- backend/pkg/routes/couple-routes.go
+++ backend/pkg/routes/couple-routes.go
@@ -8,5 +8,8 @@ import (
 var RegisterCoupleRoutes = func(router *mux.Router) {
 	router.HandleFunc("/create-couple", controllers.CreateCoupleHandler).Methods("POST")
 	router.HandleFunc("/update-couple", controllers.UpdateCoupleHandler).Methods("POST")
+	router.HandleFunc("/getcouple/{id}", controllers.GetCoupleByIDHandler).Methods("GET")
+	router.HandleFunc("/get-couple/{id}", controllers.GetCoupleByUserIDHandler).Methods("GET")
+
 }
 
